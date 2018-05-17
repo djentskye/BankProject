43b3d3535d13474cd8e94ac2e06eebe8bb6ba382
TheRockstarr33 <drockstarr33@yahoo.com>
2018-05-17 14:24:57 +0400

    Finished 'Get card balance' service

--- bankproject/src/main/java/com/hibernate/service/CardService.java
+++ bankproject/src/main/java/com/hibernate/service/CardService.java
@@ -5,6 +5,8 @@ import com.hibernate.persistence.HibernateUtil;
 import org.hibernate.Query;
 import org.hibernate.Session;
 
+import java.util.List;
+
 public class CardService {
 
     public Card getCardByUserId() {
@@ -22,22 +24,47 @@ public class CardService {
     public double getCardBalanceByCardId(int id) {
         int a;
 
+        double balance = -4;
+//        Session session = HibernateUtil.getSessionFactory().openSession();
+//        Transaction tx = null;
+
+
         try {
             Session session = HibernateUtil.getSessionFactory().openSession();
 
             session.beginTransaction();
 
-            Query query = session.createQuery("SELECT `balance` FROM `card` WHERE `id`=" + id);
+            Query query = session.createQuery("SELECT dBalance FROM Card WHERE id=" + id);
 
-            System.out.println(query);
-            double balance = -4;
+            List<Double> cards = session.createQuery("SELECT dBalance FROM Card WHERE id=" + id).list();
+
+            balance = cards.get(0);
 
             session.getTransaction().commit();
-
-            return balance;
         } catch (Exception e) {
+            System.out.println(e);
             System.out.println("There was an error processing the request. ");
-            return Integer.parseInt(null);
+//            return Integer.parseInt(null);
         }
+        return balance;
+
+
+//        try {
+//            tx = session.beginTransaction();
+//            List<Card> cards = session.createQuery("SELECT dBalance FROM Card WHERE id=" + id).list();
+////            for (Iterator iterator = employees.iterator(); iterator.hasNext();){
+////                Employee employee = (Employee) iterator.next();
+////                System.out.print("First Name: " + employee.getFirstName());
+////                System.out.print("  Last Name: " + employee.getLastName());
+////                System.out.println("  Salary: " + employee.getSalary());
+////            }
+//            Card card = cards.get(0);
+//            tx.commit();
+//        } catch (HibernateException e) {
+//            if (tx!=null) tx.rollback();
+//            e.printStackTrace();
+//        } finally {
+//            session.close();
+//        }
     }
 }
